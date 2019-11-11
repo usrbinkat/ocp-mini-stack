6e2805e2564fb02bd8069e77e0e15b04daa648fc
usrbinkat <usrbinkat@containercraft.io>
2019-11-11 07:03:04 +0000

    wip ocp4 lab poc seeds continued

--- httpd/html/src/stage-pull-secrets.sh
+++ httpd/html/src/stage-pull-secrets.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # Write script to prompt user for pull secret so secrets file can be created safely
+# https://cloud.redhat.com/openshift/install/metal/user-provisioned
 mkdir -p ~/.ccio/ocp-mini-stack
 cat <<EOF >~/.ccio/ocp-mini-stack/secrets
 secret_SSHPUBKEY=\'$(cat ~/.ssh/id_rsa.pub)\'
