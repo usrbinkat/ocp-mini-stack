adbae461aa1403dad4effe1d10821739f1b76e4a
usrbinkat <usrbinkat@braincraft.io>
2019-11-17 07:32:24 +0000

    ongoing pre-alpha poc work

--- 03_httpd/html/src/stage-pull-secrets.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Write script to prompt user for pull secret so secrets file can be created safely
-# https://cloud.redhat.com/openshift/install/metal/user-provisioned
-mkdir -p ~/.ccio/ocp-mini-stack
-cat <<EOF >~/.ccio/ocp-mini-stack/secrets
-secret_SSHPUBKEY=\'$(cat ~/.ssh/id_rsa.pub)\'
-secret_PULLSECRET=\'\'
-EOF
